c9df0cba0ce8cce8ec643e5a6db29642fe424eed
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-08-03 16:40:01 +0300

    Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace RtspStreamApp {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(0, 0);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(81, 0);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Controls.Add(this.stopButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 469);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(453, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(485, 469);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using Vlc.DotNet.Forms;
+
+namespace RtspStreamApp {
+    public partial class Form1 : Form {
+       
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            InitializeVlcControl();
+        }
+
+        private VlcControl vlcControl;
+        private VlcControl vlcControl1;
+
+        private void InitializeVlcControl() {
+            vlcControl = new VlcControl();
+            vlcControl1 = new VlcControl();
+
+          var vlcArgs = new List<string>
+    {
+        "--no-plugins-cache",
+        "--no-video-title-show",
+        "--no-snapshot-preview",
+        "--file-caching=0",
+        "--clock-jitter=0",
+        "--network-caching=80",
+        "--live-caching=0",
+        "--no-overlay",
+        "--clock-jitter=0"
+    };
+
+
+            vlcControl.BeginInit();
+            vlcControl.VlcMediaplayerOptions = vlcArgs.ToArray();
+            vlcControl.VlcLibDirectory = new DirectoryInfo(@"C:\Program Files (x86)\VideoLAN\VLC"); // VLC kütüphane dizini
+            vlcControl.EndInit();
+            vlcControl.Dock = DockStyle.Fill;
+            panel1.Controls.Add(vlcControl); // RTSP yayınını göstermek için bir Panel kontrolü ekleyin
+
+
+            vlcControl1.BeginInit();
+            vlcControl1.VlcMediaplayerOptions = vlcArgs.ToArray();
+            vlcControl1.VlcLibDirectory = new DirectoryInfo(@"C:\Program Files (x86)\VideoLAN\VLC"); // VLC kütüphane dizini
+            vlcControl1.EndInit();
+            vlcControl1.Dock = DockStyle.Fill;
+            panel2.Controls.Add(vlcControl1); // RTSP yayınını göstermek için bir Panel kontrolü ekleyin
+        }
+
+        private void StartCapture(string rtspUrl) {
+            if (vlcControl != null) {
+                vlcControl.Stop(); // Önceki medya durduruluyor
+                vlcControl.Play(new Uri(rtspUrl));
+            }
+
+     
+        }
+         
+        private void StartRightVideo(string rtspUrl) {
+            if (vlcControl1 != null) {
+                vlcControl1.Stop(); // Önceki medya durduruluyor
+                vlcControl1.Play(new Uri(rtspUrl));
+            }
+        }
+
+        private void StopCapture() {
+            if (vlcControl != null) {
+                vlcControl.Stop();
+            }
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e) {
+            string rtspUrl = "rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?";
+            string rtspUrl1 = "rtsp://192.168.1.10:554/user=admin&password=&channel=3&stream=0.sdp?";
+            StartCapture(rtspUrl);
+            StartRightVideo(rtspUrl1);
+
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e) {
+            StopCapture();
+
+
+        }
+    }
+}
